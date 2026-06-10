b9ee43705be1a503586183ee667e228d3fe4648c
vinicius200019 <41840495+vinicius200019@users.noreply.github.com>
2026-06-09 22:08:48 -0300

    Delete php/api directory

--- php/api/api_produtos.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-include_once '../db/produtos_db.php';
-
-// Aceita ?ativo=0 ou ?ativo=1 (padrão: 1 = ativos)
-$ativo = isset($_GET['ativo']) ? intval($_GET['ativo']) : 1;
-$produtos = listarProdutos($ativo);
-echo json_encode($produtos);
--- php/api/api_vendas.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/json');
-include_once '../db/vendas_db.php';
-
-$vendas = listarTodasVendas();
-echo json_encode($vendas);
